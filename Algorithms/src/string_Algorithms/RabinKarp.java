fe9df823423328bcff9e8527be5f389b94dbf16f
AKSHAY KUMAR MISHRA <akshayk.kvhec@gmail.com>
2020-07-03 23:43:05 +0530

    Time complexity updated in RabinKarp

--- Algorithms/src/string_Algorithms/RabinKarp.java
+++ Algorithms/src/string_Algorithms/RabinKarp.java
@@ -55,6 +55,17 @@
         d: Number of characters in the alphabet
         q: A prime number
         h: d^(m-1) 
+        
+        
+        
+        Time Complexity: The average and best case running 
+        time of the Rabin-Karp algorithm is O(n+m), but its 
+        worst-case time is O(nm). Worst case of Rabin-Karp 
+        algorithm occurs when all characters of pattern and 
+        text are the same as the hash values of all the 
+        substrings of txt[] match with the hash value of pat[].
+         For example pat[] = "AAA" and txt[] = "AAAAAAA".
+
  *    
  * 
  * */
